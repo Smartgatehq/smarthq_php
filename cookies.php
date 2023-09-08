afc6e5f25cf7eb68064df475b2a260e3be3d30f3
Smartgatehq <smartgate.techhq@gmail.com>
2023-09-08 11:46:18 +0100

    adding new fies

---
+++ cookies.php
@@ -0,0 +1,27 @@
+<?php
+$cookie_name = "user";
+$cookie_value = "John Doe";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Cookies</title>
+</head>
+<body>
+    <h1>PHP Cookies</h1>
+
+    <?php
+if(!isset($_COOKIE[$cookie_name])) {
+  echo "Cookie named '" . $cookie_name . "' is not set!";
+} else {
+  echo "Cookie '" . $cookie_name . "' is set!<br>";
+  echo "Value is: " . $_COOKIE[$cookie_name];
+}
+?>
+</body>
+</html>
